9ca2a535c3ef6258a343930c7757cd030112fdba
mwebazavictor <mwebazavictorandrew@gmail.com>
2024-12-30 21:14:26 +0300

    Add events app and configure URLs and settings

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return HttpResponse("We'll render an events page here.")
